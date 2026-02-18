af161e8046ededf9fec83b187b7fd1d09154e490
Jona615m <joole24@student.sdu.dk>
2026-02-18 11:21:19 +0100

    im getting a crashout

--- Main/src/main/java/dk/sdu/mmmi/cbse/Main.java
+++ Main/src/main/java/dk/sdu/mmmi/cbse/Main.java
@@ -1,11 +1,25 @@
 package dk.sdu.mmmi.cbse;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+
+public class Main extends Application {
 
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        StackPane root = new StackPane();
+        var scene = new Scene(root, 800, 600);
+
+        window.setTitle("CBSE Game");
+        window.setScene(scene);
+        window.show();
     }
 }

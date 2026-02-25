ad072776386e24951bd99c5712eead79723f4472
Jona615m <joole24@student.sdu.dk>
2026-02-25 10:57:15 +0100

    im getting a crashout

--- Main/src/main/java/dk/sdu/mmmi/cbse/Main.java
+++ Main/src/main/java/dk/sdu/mmmi/cbse/Main.java
@@ -4,22 +4,43 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
 
 public class Main extends Application {
 
+    @Override
+    public void start(Stage window) throws Exception {
+        StackPane root = new StackPane();
+
+        try {
+            Image backgroundImage = new Image(getClass().getResourceAsStream("/spaceprojekt.png"));
+            ImageView backgroundImageView = new ImageView(backgroundImage);
+
+            backgroundImageView.setFitWidth(800);
+            backgroundImageView.setFitHeight(600);
+            backgroundImageView.setPreserveRatio(false);
+
+            root.getChildren().add(backgroundImageView);
+
+        } catch (Exception e) {
+            System.out.println("Error loading image: " + e.getMessage());
+            return;
+        }
+
+
+        var scene = new Scene(root, 800, 600);
+
+        window.setTitle("Jonas' crazy space project");
+        window.setScene(scene);
+        window.show();
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
-    public void start(Stage window) throws Exception {
-        StackPane root = new StackPane();
-        var scene = new Scene(root, 800, 600);
 
-        window.setTitle("CBSE Game");
-        window.setScene(scene);
-        window.show();
-    }
 }
---
+++ Main/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module Main {
+    requires javafx.graphics;
+    opens dk.sdu.mmmi.cbse to javafx.graphics;
+}
